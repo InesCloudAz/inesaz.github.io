4476c055e9ca90073532b57615a03da44eeba61d
Ines Azouz <inesazouz@gmail.com>
2026-06-02 10:26:34 +0200

    Clean up CV repository

--- Todo.Application/Abstractions/IAzureAiSearchTodoSearchService.cs
+++
@@ -1,9 +0,0 @@
-using Todo.Application.Search;
-
-namespace Todo.Application.Abstractions;
-
-public interface IAzureAiSearchTodoSearchService
-{
-    bool IsConfigured { get; }
-    Task<IReadOnlyList<TodoSearchDocument>> SearchTodosAsync(string searchTerm, string userId, CancellationToken cancellationToken = default);
-}
--- Todo.Infrastructure/Identity/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Todo.Infrastructure.Identity;
-
-public class ApplicationUser : IdentityUser
-{
-    public string DisplayName { get; set; } = string.Empty;
-}
